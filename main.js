14b149f2e6d6e6f51aeb79a9660e04d2b05ecb34
Chaslaw <caslavlazic@gmail.com>
2019-11-29 19:12:00 +0100

    js code of the first 3 tasks

---
+++ main.js
@@ -0,0 +1,30 @@
+//Task No 1
+
+var Peter = 3500;
+var Jim = 5000; 
+
+console.log (Peter > Jim);
+
+//Task No 2
+
+var John = 38;
+var Mike = (John + 8)/2; //moglo je i sve u jednoj liniji (John + 8)/2-8; ali                               sam ga malo zakomplikovao cisto radi vezbe :)
+var nowMike = Mike - 8;
+
+Mike = nowMike;
+
+console.log (Mike);
+
+//Task No 3
+
+var firstFlock = 78;
+var secondFlock = 57;
+
+firstFlock -= 12;
+secondFlock += 12;
+
+var finalSecondFlock = secondFlock - 4;
+
+console.log (firstFlock >= finalSecondFlock);
+
+
