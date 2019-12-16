c17e3ed96f2e16f8229a717cd0e9f4b8e8a81e7d
Chaslaw <57915591+Chaslaw@users.noreply.github.com>
2019-12-16 18:33:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-//Task No 1
+//TASK 1 :
+//Peter has 3500$, and Jim has 5000$, print if Peter has more money than Jim (console log result, true of false)
+
 
 var Peter = 3500;
 var Jim = 5000; 
 
 console.log (Peter > Jim);
 
-//Task No 2
+//TASK 2 :
+//John is 38 years old. In 8 years John will be two times older than Mike. How old is Mike now (console log result)
+
 
 var John = 38;
 var Mike = (John + 8)/2; //moglo je i sve u jednoj liniji (John + 8)/2-8; ali                               sam ga malo zakomplikovao cisto radi vezbe :)
@@ -15,7 +19,11 @@ Mike = nowMike;
 
 console.log (Mike);
 
-//Task No 3
+//TASK 3 :
+//There are 78 birds in a first flock, and 57 birds in a second flock. 
+//12 birds decided to move from the first flock to the second flock, 
+//and then 4 birds left second flock. 
+//Is the first flock larger or equal to the second one ? (console log result, true or false)
 
 var firstFlock = 78;
 var secondFlock = 57;
