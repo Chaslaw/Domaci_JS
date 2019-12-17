0a48335a0087519cee9e8c337abc6c29820eea8e
Chaslaw <caslavlazic@gmail.com>
2019-12-17 20:39:59 +0100

    final version

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-    
     //Task 6 (Odabrao sam dva razlicita niza, iako je 
     //mogao da bude i isti za obe)
 
@@ -37,7 +35,7 @@
         //Task 7
          
            /* Petlju za pretragu jedinstvenih brojeva prvo sam uradio ovako,
-            videvsi kako se radi na netu, dakle nije moja originalna ideja. Kako to nije davalo zeljeni rezultat, presao sam na drugu petlju koja sledi. Ovo sam ostavio jer je korisno, i mozda zatreba kasnije.
+            videvsi kako se radi na netu, dakle nije moja originalna ideja. Kako to nije davalo zeljeni rezultat (izbacivao je samo duplikate a ostavljao originale), presao sam na drugu petlju koja sledi dole. Ovo sam ostavio jer je korisno, i mozda mi zatreba nekad kasnije.
 
             var unique = [];
             var b = false; 
@@ -57,12 +55,14 @@
        }
 
        */
-    
 
+    //    Dalje sledi kompletan zadatak
+    // Nisam siguran da li je trebalo ovako
+    // Prilicno sumnjam da je ovo trazeno u zadatku  
+    // Dugo sam razmisljao i na kraju ga ovako uradio...
 
-  var deleteBiggest = function (arr){
-        
-
+    var findUnqiue = function (arr){
+     
     var order = [];
     var unique = [];
     
@@ -78,31 +78,48 @@
     }
 
     for (var i=0; i<arr.length; i++){
-        
         for (var j=0; j<arr.length; j++){
             if(arr[j]!=false){
                 if(min>arr[j]){
                     min=arr[j];
                     position=j;
                 }
-
             }
-          
         }
         order[i] = min; 
         arr[position]=false;
         min=max;
     }
-       //Petlja za izbacivanje originala i duplikata
+       //Petlja za izbacivanje i originala i duplikata
     for (var i=0; i<order.length; i++){
         if (order[i]!=order[i-1] && order[i]!=order[i+1]){
             unique[i]=order[i];
         }
     }
 
-  
   console.log(unique);
-            
+  return unique;
+}
+//Vec sam prethodno poredjao niz od najmanjeg ka najvecem
+// pa mi je sad znatno lakse da izbacim najveci
+    
+    function deleteBiggest (arr) {
+
+        var newArr = [];
+
+        for(var i=0; i<arr.length; i++){
+            newArr[i]=arr[i-1];
+        }
+        return newArr;
     }
- 
-    deleteBiggest([15, 35, 46, 23, 15, 17, 23, 24, 35, 12, 72, 64, 35, 22, 64]);
+    
+    function finalProduct (arr, deleteBiggest){
+
+        var a = findUnqiue(arr);
+        var b = deleteBiggest(a);
+        console.log(b);
+    }
+
+    finalProduct([15, 35, 46, 23, 15, 17, 23, 24, 35, 12, 72, 64, 35, 22, 64], deleteBiggest);
+
+
