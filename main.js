da6a8dcc92389d2625378eac23f5e63cbf60dec9
Chaslaw <caslavlazic@gmail.com>
2019-12-23 14:33:06 +0100

    object inheritance

--- main.js
+++ main.js
@@ -1,130 +1,49 @@
-// TASK 1: 
-// Based on existing object:
-// Create a function which should change age to 34, and delete married property. In the end console.log object.
-
-var person = {
-	name: "Mike",
-	age: 28,
-	married: true
-}
-
-function changeDelete () {
-    person.age = 34;
-    delete person.married;
-
-    console.log(person);
-}
-
-changeDelete();
-
-
-// TASK 2: 
-
-// Based on existing object:
-// Create a function which should check if person has children property, if not add it. Its value should be array containing two objects with children data. Each object should contain child name, age and gender.
-
+// TASK 5:
+// Create the other object which should inherit data from existing object. Then console.log age by accessing the newly created object.
 
 var person = {
 	name: "Jack",
 	age: 32,
 	married: true
-}
+};
+
+var other = Object.create (person);
+
+console.log(other.age);
 
 
+// Task6
+// Create a method which, when called, should console.log name and age of existing object.
 
-function child () {
-    
-    if (typeof person.children===null){
-        console.log(person);
+var person = {
+	name: "Mike",
+	age: 28,
+    married: true,
+    callMe: function (){
+        console.log(this.name);
+        console.log(this.age);
+
     }
-    else { person.children=[{},{}];
- person.children[0].name="Mike";
- person.children[0].age=8;
- person.children[0].gender="male";
- person.children[1].name="Anna";
- person.children[1].age=9;
- person.children[1].gender="female";}
-
-    console.log(person);
-}
-child();
-
-// TASK 3:
-// Based on existing array of objects:
-// Create a function which should console.log if student passed exam, result should be something like:
-// "Jack passed exam" or "Mike didn't pass exam"
-
-
-var students = [ 
-    {
-        name: 'Mike',
-        age: 28,
-        passed: false
-    },
-    {
-        name: 'Anna',
-        age: 23,
-        passed: true
-    },
-    {
-        name: 'Jack',
-        age: 32,
-        passed: true
-    },
- ];
-
- function passExame (){
-
-    for (var i=0; i<students.length; i++)
-     if (students[i].passed==false){
-         console.log(students[i].name + " didn't pass exam")
-     }
-     else{
-        console.log(students[i].name + " passed exam")
-     }
- }
-
- passExame();
-
-
- // TASK 4:
-//  Create a function which should repack existing data into three arrays, names, ages, and passed. Example of names array:
-
-var students = [ 
-    {
-        name: 'Mike',
-        age: 28,
-        passed: false
-    },
-    {
-        name: 'Anna',
-        age: 23,
-        passed: true
-    },
-    {
-        name: 'Jack',
-        age: 32,
-        passed: true
-    },
- ];
-
- function repack () {
-
-    var names = [];
-    var age = [];
-    var passed = [];
-     
- for (var i=0; i<students.length; i++){
-         names[i]=students[i].name;
-         age[i]=students[i].age;
-         passed[i]=students[i].passed;
-     }
-     console.log(names);
-     console.log(age);
-     console.log(passed);
 }
 
-repack();
+person.callMe();
+
+// TASK 7
+// Create a method which should create name, age and married properties in existing object, based on parameters passed in method.
+
+
+var person = {
+    newData: function (){
+        this.name="Mike";
+        this.age=36;
+        this.married=false;
+    }
+}
+person.newData();
+
+
+
+
 
 
 
